8e5bfe2558d0c860e241e75964090ceb0e145d94
Fathurozak Buhari <rozakbuhari@gmail.com>
2020-02-19 09:59:17 +0700

    Setup emotion for stylings

--- webpack.config.js
+++ webpack.config.js
@@ -17,10 +17,7 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
+          loader: 'babel-loader'
         }
       }
     ]
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -22,10 +22,7 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
-          },
+          loader: 'babel-loader'
         },
       },
     ],
