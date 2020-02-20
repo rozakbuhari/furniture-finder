7c4b6336f5ab6e97a0bcaec49c11ea824ac4c9dc
Fathurozak Buhari <rozakbuhari@gmail.com>
2020-02-20 11:34:32 +0700

    Add Layout to ProductPage

--- src/pages/Products/index.js
+++ src/pages/Products/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import Layout from '../../components/Layout';
 import { fetchProducts } from '../../redux/actions/products';
 import {
   getProductsLoading,
@@ -24,13 +25,13 @@ const ProductsPage = () => {
   }, []);
 
   return (
-    <div>
+    <Layout>
       <ProductFilter
         loading={furnitureStylesLoading}
         furnitureStyles={furnitureStyles}
       />
       <ProductList products={products} loading={productsLoading} />
-    </div>
+    </Layout>
   );
 };
 
